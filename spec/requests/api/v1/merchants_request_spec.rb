25c8a7c76df82873f5e84eac1330fabb95cfc23c
Amy Spears <amspears007@gmail.com>
2023-07-11 15:50:28 -0600

    create new test fo merchant items request and update routes

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -28,6 +28,7 @@ RSpec.describe "Merchants API" do
 end
 
   it "finds a single merchant by its id" do
+  
     id = create(:merchant).id
     get "/api/v1/merchants/#{id}"
     
@@ -42,6 +43,15 @@ end
     expect(merchant[:data].count).to eq(3)
     expect(merchant[:data][:attributes]).to have_key(:name)
     expect(merchant[:data][:attributes][:name]).to be_a(String)
+  end
 
+  it "return items associated with a specific merchant" do
+    merchant1 = Merchant.create(name: "Amy")
+    item1 = merchant1.items.create!(name: "Rubber Chicken", description: "Its funny anf bouncy", unit_price: 10.0)
+    # require 'pry'; binding.pry
+    get "/api/v1/merchants/#{merchant1.id}/items"
+
+    expect(response).to be_successful
+    
   end
 end
