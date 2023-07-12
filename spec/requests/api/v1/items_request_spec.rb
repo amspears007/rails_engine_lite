f0ed7ac190ce0ef93b2e17b7a98e05543a8e99b4
Amy Spears <amspears007@gmail.com>
2023-07-11 19:40:03 -0600

    Add get response with route and show action added to controller

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,11 +1,13 @@
 require "rails_helper"
 
 RSpec.describe "Items API" do
+  before(:each) do
+    @merchant1 = Merchant.create(name: "Amy")
+    @item1 = @merchant1.items.create!(name: "Rubber Chicken", description: "Its funny and bouncy", unit_price: 10.0)
+    @item2 = @merchant1.items.create!(name: "Actual Chicken", description: "It clucks", unit_price: 25.0)
+  end
   describe "return all items" do
     it "exposes the endpoint for all items" do
-      merchant1 = Merchant.create(name: "Amy")
-      item1 = merchant1.items.create!(name: "Rubber Chicken", description: "Its funny and bouncy", unit_price: 10.0)
-      item2 = merchant1.items.create!(name: "Actual Chicken", description: "It clucks", unit_price: 25.0)
       get "/api/v1/items"
 
     expect(response).to be_successful
@@ -19,7 +21,15 @@ RSpec.describe "Items API" do
     expect(items[:data][1]).to have_key(:id)
     expect(items[:data][1][:attributes][:description]).to eq("It clucks")
     expect(items[:data][1][:attributes][:unit_price]).to eq(25.0)
-    # require 'pry'; binding.pry
+    end
+  end
+  
+  describe "returns one item by id" do
+    it "finds item by id '/api/v1/items/:item_id" do
+      get "/api/v1/items/#{@item2.id}"
+
+    expect(response).to be_successful
+
     end
   end
 end
