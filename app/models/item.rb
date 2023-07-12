9216916c6f841bf852a4aa740a02d2af2f64ab16
Amy Spears <amspears007@gmail.com>
2023-07-12 12:04:13 -0600

    Add validationd to merchant and item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,9 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  validates_presence_of :name,
+                        :unit_price,
+                        :description,
+                        :merchant_id
 end
