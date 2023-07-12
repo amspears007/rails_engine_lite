8fa9e2e90114180f8a3f98632883d8cfdd4216ce
Amy Spears <amspears007@gmail.com>
2023-07-11 19:32:54 -0600

    Add additional tests to complete /api/v1/items request

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -3,12 +3,23 @@ require "rails_helper"
 RSpec.describe "Items API" do
   describe "return all items" do
     it "exposes the endpoint for all items" do
-    get "/api/v1/items"
+      merchant1 = Merchant.create(name: "Amy")
+      item1 = merchant1.items.create!(name: "Rubber Chicken", description: "Its funny and bouncy", unit_price: 10.0)
+      item2 = merchant1.items.create!(name: "Actual Chicken", description: "It clucks", unit_price: 25.0)
+      get "/api/v1/items"
 
     expect(response).to be_successful
-    # expect(response.status).to eq(200)
+    expect(response.status).to eq(200)
 
-    # items = JSON.parse(response.body, symbolize_names: true)
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items[:data]).to be_a(Array)
+    expect(items[:data].count).to eq(2)
+    expect(items[:data][1]).to be_a(Hash)
+    expect(items[:data][1]).to have_key(:id)
+    expect(items[:data][1][:attributes][:description]).to eq("It clucks")
+    expect(items[:data][1][:attributes][:unit_price]).to eq(25.0)
+    # require 'pry'; binding.pry
     end
   end
 end
