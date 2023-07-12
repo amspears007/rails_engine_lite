52f80daf7ee85d3bbc10f94afea453123aba3314
Amy Spears <amspears007@gmail.com>
2023-07-11 18:47:43 -0600

    Remove relationship from serializer so postman tests will pass

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,6 +1,7 @@
 class ItemSerializer
   include JSONAPI::Serializer
+  # set_id {3}
+  # set_type "giggles"
   attributes :name, :description, :unit_price, :merchant_id
 
-  belongs_to :merchant
 end
