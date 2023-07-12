2504d2fcafa88a59ed7a3cf6e6cc4f44ab8c7f5d
Amy Spears <amspears007@gmail.com>
2023-07-11 19:19:36 -0600

    Add index route and controller

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -2,13 +2,13 @@ require "rails_helper"
 
 RSpec.describe "Items API" do
   describe "return all items" do
-    xit "exposes the endpoint for all items" do
+    it "exposes the endpoint for all items" do
     get "/api/v1/items"
 
     expect(response).to be_successful
-    expect(response.status).to eq(200)
+    # expect(response.status).to eq(200)
 
-    items = JSON.parse(response.body, symbolize_names: true)
+    # items = JSON.parse(response.body, symbolize_names: true)
     end
   end
 end
