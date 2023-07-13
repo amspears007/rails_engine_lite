f0bb68e1c4ff41eb45cd3b98b9f3be7e1f53dfe0
Amy Spears <amspears007@gmail.com>
2023-07-12 20:18:02 -0600

    set up spec for items search.  Add data and figure out endpoint for request

---
+++ spec/requests/api/v1/search_items_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe "Items Search API" do
+  describe "it searches for a single item that matches a serch term" do
+    it "GET /api/vi/items/find" do
+      merchant1 = Merchant.create(name: "Amy")
+      merchant2 = Merchant.create(name: "Vivian")
+      merchant3 = Merchant.create(name: "Penny")
+      merchant4 = Merchant.create(name: "Amanda")
+
+
+      item1 = merchant1.items.create!(name: "Rubber Chicken", description: "Its funny and bouncy", unit_price: 10.0)
+      item2 = merchant1.items.create!(name: "Actual Chicken", description: "It clucks", unit_price: 25.0)
+      item3 = merchant2.items.create!(name: "Paint Brush", description: "You paint with it", unit_price: 5.0)
+      item4 = merchant2.items.create!(name: "Purple Paint", description: "Its color is purple", unit_price: 15.12)
+      item5 = merchant2.items.create!(name: "Markers", description: "many colors", unit_price: 3.12)
+      item6 = merchant3.items.create!(name: "Orange Paint", description: "Its color is orange", unit_price: 10.12)
+
+      get "/api/v1/items/find?name=amy"
+      expect(response).to be_successful
+      search_array = JSON.parse(response.body, symbolize_names: true)
+      
+
+    end
+  end
+end
