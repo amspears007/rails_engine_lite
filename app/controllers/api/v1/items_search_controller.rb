4e99ec04c8e47c15c414c8e2ad64edd3a7baf68f
Amy Spears <amspears007@gmail.com>
2023-07-13 19:29:12 -0600

    Add conditional to search action

--- app/controllers/api/v1/items_search_controller.rb
+++ app/controllers/api/v1/items_search_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::ItemsSearchController < ApplicationController
   def search
-   
+    if params[:name].present?
+      found_item = Item.find_item_by_name_search(params[:name])
+      render json: ItemSerializer.new(found_item)
+     
+    else
+      render json: item.errors, status: 404
+
+    end
+ 
   end
 end
 
